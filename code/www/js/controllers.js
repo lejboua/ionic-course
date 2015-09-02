042998aa51eb3838befc9cf3ed320d6bdbf7afb0
André Albuquerque <andre.m.de.albuquerque@gmail.com>
2015-09-02 17:09:09 +0100

    First part of the tutorial done (until Wiring Up to A Server)

--- code/www/js/controllers.js
+++ code/www/js/controllers.js
@@ -4,16 +4,66 @@ angular.module('songhop.controllers', ['ionic', 'songhop.services'])
 /*
 Controller for the discover page
 */
-.controller('DiscoverCtrl', function($scope) {
+.controller('DiscoverCtrl', function($scope, $timeout, User) {
+    // our first three songs
+    $scope.songs = [
+        {
+            "title":"Stealing Cinderella",
+            "artist":"Chuck Wicks",
+            "image_small":"https://i.scdn.co/image/d1f58701179fe768cff26a77a46c56f291343d68",
+            "image_large":"https://i.scdn.co/image/9ce5ea93acd3048312978d1eb5f6d297ff93375d"
+        },
+        {
+            "title":"Venom - Original Mix",
+            "artist":"Ziggy",
+            "image_small":"https://i.scdn.co/image/1a4ba26961c4606c316e10d5d3d20b736e3e7d27",
+            "image_large":"https://i.scdn.co/image/91a396948e8fc2cf170c781c93dd08b866812f3a"
+        },
+        {
+            "title":"Do It",
+            "artist":"Rootkit",
+            "image_small":"https://i.scdn.co/image/398df9a33a6019c0e95e3be05fbaf19be0e91138",
+            "image_large":"https://i.scdn.co/image/4e47ee3f6214fabbbed2092a21e62ee2a830058a"
+        }];
+    // initialize the current song (angular.copy performs a deep copy)
 
+    var currentIndex = -1;
+    var setCurrentSong = function(indx){
+        currentIndex = indx;
+        console.log("setting current song! ", currentIndex);
+        $scope.currentSong = angular.copy($scope.songs[indx]);
+    };
+
+    setCurrentSong(0);
+
+    $scope.sendFeedback = function (bool) {
+        if (bool) User.addSongToFavorites($scope.currentSong);
+
+        $scope.currentSong.rated = bool;
+        $scope.currentSong.hide = true;
+
+        $timeout(function() {
+            // $timeout to allow animation to complete before changing to next song
+            // set the current song to one of our three songs
+            var randomSong = -1;
+            do {
+                randomSong = Math.round(Math.random() * ($scope.songs.length - 1));
+            } while (randomSong === currentIndex);
+
+            setCurrentSong(randomSong);
+        }, 250);
+    }
 })
 
 
 /*
 Controller for the favorites page
 */
-.controller('FavoritesCtrl', function($scope) {
-
+.controller('FavoritesCtrl', function($scope, User) {
+    $scope.favorites = User.favorites;
+    $scope.removeSong = function(song, index) {
+        User.removeSongFromFavorites(song, index);
+    }
 })
 
 
--- code/www/js/services.js
+++ code/www/js/services.js
@@ -1,1 +1,28 @@
-angular.module('songhop.services', []);
+angular.module('songhop.services', [])
+.factory ('User', function() {
+    var o = {
+        favorites: []
+    };
+    o.addSongToFavorites = function(song) {
+        if (!song) return false;
+        console.log('favoriting! ', song.title);
+        o.favorites.unshift(song);
+    }
+
+    o.removeSongFromFavorites = function(song, index) {
+        if (!song) return false;
+        console.log('removing! ', song.title);
+        o.favorites.splice(index, 1);
+    }
+
+    return o;
+})
+
+.service ('UserService', function UserFactory() {
+    this.favorites = [];
+    this.addSongToFavorites = function(song) {
+        if (!song) return false;
+        console.log('favoriting in service! ', song.title);
+        this.favorites.unshift(song);
+    }
+});
